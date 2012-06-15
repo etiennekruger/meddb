73a75f9d536b41593a4394e6bec89347a3e52ddb
Etienne Kruger <etienne@acscomputers.co.za>
2012-06-15 16:46:27 +0200

    Added client side search capability.

--- frontend/js/meddb.alpha.js
+++ frontend/js/meddb.alpha.js
@@ -234,7 +234,7 @@ meddb.tabber.change = function(tab) {
 	.classed('active', true);
 }
 meddb.medicine = {};
-meddb.medicine.list = function() {
+var medicine_list = function(data, node) {
     var row_data = function(d) {
 	var row = [];
 	var formulation = [];
@@ -250,27 +250,55 @@ meddb.medicine.list = function() {
 	row.push(d3.round(d.mshprice,4) || '');
 	return row;
     }
+    var tbody = d3.select(node)
+	.select('#meddb_medicine_list')
+	.select('tbody');
+    tbody.selectAll('tr')
+	.remove();
+    var rows = tbody.selectAll('tr')
+	.data(data)
+	.enter()
+	.append('tr')
+	.style('cursor', 'pointer')
+	.on('click', function(d) { location.hash = 'medicine:'+d.id; });
+    rows.selectAll('td')
+	.data(row_data)
+	.enter()
+	.append('td')
+	.text(function(d) { return d; });
+}
+
+meddb.medicine.data = {};
+meddb.medicine.list = function() {
     meddb.template.hide();
     load('/medicine_list.html', function(fragment) {
 	load('/json/medicine/', function(data) {
-	    var rows = d3.select(fragment)
-		.select('#meddb_medicine_list')
-		.select('tbody')
-		.selectAll('tr')
-		.data(data)
-		.enter()
-		.append('tr')
-		.style('cursor', 'pointer')
-		.on('click', function(d) { location.hash = 'medicine:'+d.id; });
-	    rows.selectAll('td')
-		.data(row_data)
-		.enter()
-		.append('td')
-		.text(function(d) { return d; });
+	    meddb.medicine.data = data;
+	    medicine_list(data, fragment);
 	    meddb.history.add(location.hash, 'All Medicine');
 	    meddb.template.show(fragment);
+	    d3.select('#meddb_medicine_search')
+		.on('keyup', function() { meddb.medicine.filter(this.value) });
 	});
     });
+}
+
+meddb.medicine.filter = function(filter) {
+    /* Filter the data for the live search. */
+    var filtered = [];
+    meddb.medicine.data.forEach(function(item) {
+	var match = 0;
+	item.ingredients.forEach(function(ingredient) {
+	    var inn = ingredient.inn.toLowerCase();
+	    if (inn.indexOf(filter.toLowerCase()) != -1) {
+		match++;
+	    }
+	});
+	if (match > 0) {
+	    filtered.push(item);
+	}
+    });
+    medicine_list(filtered, d3.select('div.meddb_inner_page')[0][0])
 }
 meddb.medicine.detail = function(id) {
     meddb.template.hide();
