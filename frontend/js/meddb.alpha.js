b8e3487385b3964e5f388a7128377009c941a670
Adi Eyal <adi@burgercom.co.za>
2012-07-05 22:27:47 +0200

    Country names for bar graphs now extracted dynamically

--- frontend/js/meddb.alpha.js
+++ frontend/js/meddb.alpha.js
@@ -365,43 +365,45 @@ meddb.medicine.detail = function(id) {
 		return row;
 	    }
 	    var rows = d3.select(fragment)
-		.select('#meddb_medicine_product')
-		.select('tbody')
-		.selectAll('tr')
-		.data(data.procurements)
-		.enter()
-		.append('tr');
-	    rows.selectAll('td')
-		.data(product_data)
-		.enter()
-		.append('td')
-		.text(function(d) { return d.text; })
-		.style('cursor', function(d) { if (d.hash) { return 'pointer' } })
-		.on('click', function(d) { if (d.hash) {location.hash = d.hash;} });
+            .select('#meddb_medicine_product')
+            .select('tbody')
+            .selectAll('tr')
+            .data(data.procurements)
+            .enter()
+            .append('tr');
+                rows.selectAll('td')
+                .data(product_data)
+                .enter()
+                    .append('td')
+                    .text(function(d) { return d.text; })
+                    .style('cursor', function(d) { if (d.hash) { return 'pointer' } })
+                    .on('click', function(d) { if (d.hash) {location.hash = d.hash;} });
+
 	    /* Add the medicine prices graph. */
 	    var prices_data = function() {
-		//var countries = ['DRC', 'Lesotho', 'Malawi', 'Mozambique',
-		//		 'South Africa', 'Tanzania', 'Zambia', 'Zimbabwe'];
-		var countries = ['Kenya', 'Uganda', 'Tanzania'];
-		var prices = {};
-		data.procurements.forEach(function(item) {
-		    if (typeof(prices[item.country.name]) == 'undefined') {
-			prices[item.country.name] = [];
-		    }
-		    prices[item.country.name].push(item.price);
-		});
-		graph_data = [];
-		countries.forEach(function(country) {
-		    var d = { 'key': country }
-		    if ((prices[country]) && (prices[country].length > 0)) {
-			d.value = d3.sum(prices[country]) / prices[country].length;
-		    } else {
-			d.value = 0;
-		    }
-		    graph_data.push(d);
-		});
-		return graph_data;
+            //var countries = ['Kenya', 'Uganda', 'Tanzania'];
+            var countries = data.procurements.map(function(el) { return el.country.name; });
+            var prices = {};
+            console.log(data.procurements);
+            data.procurements.forEach(function(item) {
+                if (typeof(prices[item.country.name]) == 'undefined') {
+                prices[item.country.name] = [];
+                }
+                prices[item.country.name].push(item.price);
+            });
+            graph_data = [];
+            countries.forEach(function(country) {
+                var d = { 'key': country }
+                if ((prices[country]) && (prices[country].length > 0)) {
+                    d.value = d3.sum(prices[country]) / prices[country].length;
+                } else {
+                    d.value = 0;
+                }
+                graph_data.push(d);
+            });
+            return graph_data;
 	    }
+
 	    var prices_max = function(d) {
 		var sum = 0, count = 0, max = 0;
 		d.forEach(function(i) {
