bae109e45b6bdbb5c8f61d3365b97277c637780f
Etienne Kruger <etienne@acscomputers.co.za>
2012-12-05 11:28:02 +0200

    Frontend updates.

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,5 +1,12 @@
 var settings = {
     'static_base_url': '',
     'data_base_url': '',
-    'loader': 'jsonp'
-}
+    'loader': 'standard'
+};
+
+(function(url) {
+    /* Update the settings with overrides from url. */
+    d3.json(url, function(data) {
+	settings = data;
+    });
+})('meddb.json');
