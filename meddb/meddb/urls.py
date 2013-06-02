ee7b534d3ddae99b0b5a0e429f9f20f41a8bf782
Adi Eyal <adi@burgercom.co.za>
2013-06-02 11:18:34 +0200

    Added url to report

--- meddb/meddb/urls.py
+++ meddb/meddb/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^json/', include('registrations.urls')),
+    url(r'^reports/', include('reports.urls')),
 )
