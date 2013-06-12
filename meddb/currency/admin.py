f15157cdae2b37f127868e161f17c4350fb9945b
SARPAM Administrator <sarpam@django.(none)>
2013-06-12 14:13:02 +0200

    Added additional fields to currency admin.py

--- meddb/currency/admin.py
+++ meddb/currency/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from models import Currency, ExchangeRate
 
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_filter = ('currency',)
+    list_display = ('currency', 'rate', 'date')
+
 admin.site.register(Currency)
-admin.site.register(ExchangeRate)
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
