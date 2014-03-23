737e28adc36390da119049076e0dc36236a9ac8c
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-03-23 13:46:18 +0200

    Add medicine index page.

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,22 @@
 from flask import render_template
 from frontend import app
+import requests
 
 @app.route('/')
 def root():
 
-    return render_template('index.html')
+    return render_template('index.html', active_nav_button="home")
+
+
+@app.route('/medicines')
+def medicines():
+
+    response = requests.get('http://127.0.0.1:8000/json/medicine/')
+    medicine_list = response.json()
+    return render_template('medicines.html', medicine_list=medicine_list, active_nav_button="medicines")
+
+
+@app.route('/suppliers')
+def suppliers():
+
+    return render_template('suppliers.html', active_nav_button="suppliers")
