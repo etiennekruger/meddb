bad1ab8644a035842f546d0e6d314165397821c8
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-04-01 16:42:21 +0200

    Serialization: include Source with procurement. Include Medicine with Procurement.

--- med-db/registrations/models.py
+++ med-db/registrations/models.py
@@ -18,7 +18,7 @@ class Source(models.Model):
     name = models.CharField(max_length=64)
     date = models.DateTimeField(default=datetime.datetime.now)
     url = models.URLField(blank=True, null=True, verbose_name='Source Document URL', help_text='Provide a link to the source document for reference purposes. Ideally, load the document into the Infohub CKAN installation at data.medicinesinfohub.net and add the link to the source of the document as an additional resource.')
-    
+
     def __unicode__(self):
         s = u'%s @ %s' % (self.name, self.date)
         return s
@@ -37,12 +37,12 @@ class SourcedModel(models.Model):
 class Country(models.Model):
     name = models.CharField(max_length=16)
     code = models.CharField(max_length=3)
-    
+
     def as_dict(self, minimal=False):
         return { 'id': self.id,
                  'name': self.name,
                  'code': self.code }
-    
+
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.code.upper())
 
@@ -52,35 +52,35 @@ class Country(models.Model):
 class Incoterm(models.Model):
     name = models.CharField(max_length=3)
     description = models.CharField(max_length=128)
-    
+
     def as_dict(self, minimal=False):
         return {"name": self.name, "description": self.description}
-    
+
     def __unicode__(self):
         return u'%s' % (self.name)
 
 class DosageForm(models.Model):
     name = models.CharField(max_length=16)
-    
+
     def as_dict(self, minimal=False):
         return self.name
-    
+
     def __unicode__(self):
         return u'%s' % (self.name)
 
 class INN(models.Model):
     name = models.CharField(max_length=128)
-    
+
     def as_dict(self, minimal=False):
         d = { 'id': self.id,
               'name': self.name }
         if not minimal:
             d['medicines'] = [m.as_dict(minimal=True) for m in self.medicine_set.all()]
         return d
-    
+
     def __unicode__(self):
         return u'%s' % (self.name)
-    
+
     class Meta:
         verbose_name = 'INN'
         verbose_name_plural = 'INNs'
@@ -94,18 +94,18 @@ class Medicine(models.Model):
     ingredients = models.ManyToManyField(INN, through='Ingredient')
     dosageform = models.ForeignKey(DosageForm)
     name = models.CharField(max_length=50, null=True, blank=True)
-    
+
     @property
     def actives(self):
         return ', '.join([str(i) for i in self.ingredient_set.all()])
-    
+
     @property
     def msh(self):
         try:
             return self.mshprice.price
         except:
             return None
-    
+
     def as_dict(self, products=True, minimal=False, procurements=True):
         if minimal:
             d = {
@@ -114,12 +114,8 @@ class Medicine(models.Model):
                 'dosageform': { 'id': self.dosageform.id,
                                 'name': self.dosageform.name },
                 'avgprice': self.avgprice(),
-                'mshprice': self.msh,
-                'products': [{
-                        'id': p.id,
-                        'name': p.name
-                        } for p in self.product_set.all()]
-                }
+                'mshprice': self.msh
+            }
             if self.name:
                 d['name'] = self.name.capitalize()
             return d
@@ -134,7 +130,7 @@ class Medicine(models.Model):
         if products:
             d['products'] = [p.as_dict(medicine=False, minimal=minimal) for p in self.product_set.all()]
         return d
-    
+
     def avgprice(self):
         sum = 0
         tot = 0
@@ -145,7 +141,7 @@ class Medicine(models.Model):
         if tot > 0:
             return sum/tot
         return None
-    
+
     def __unicode__(self):
         if self.name:
             return u'%s %s' % (self.name, self.dosageform)
@@ -155,15 +151,15 @@ class Ingredient(models.Model):
     medicine = models.ForeignKey(Medicine)
     inn = models.ForeignKey(INN)
     strength = models.CharField(max_length=16)
-    
+
     def as_dict(self, minimal=False):
         return { 'id': self.inn.id,
                  'inn': self.inn.name.capitalize(),
                  'strength': self.strength }
-        
+
     def __unicode__(self):
         return u'%s %s' % (self.inn, self.strength)
-    
+
     class Meta:
         ordering = ('inn__name',)
 
@@ -175,7 +171,7 @@ class Ingredient(models.Model):
 class MSHPrice(models.Model):
     medicine = models.OneToOneField(Medicine)
     price = models.FloatField(help_text='The MSH price for the medicine.')
-    
+
     def __unicode__(self):
         return u'%s @ %.4f' % (self.medicine, self.price)
 
@@ -183,7 +179,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=64, verbose_name='Manufacturer Name')
     country = models.ForeignKey(Country, blank=True, null=True)
     website = models.URLField(blank=True, null=True, help_text="e.g. http://www.example.com, ensure that the leading http:// is included")
-    
+
     def as_dict(self, minimal=False):
         d = { 'id': self.id,
               'name': self.name,
@@ -193,7 +189,7 @@ class Manufacturer(models.Model):
             d['sites'] = [s.as_dict(minimal=True) for s in self.site_set.all()]
             d['suppliers'] = [s.as_dict(minimal=True, manufacturer=False) for s in self.supplier_set.all()]
         return d
-    
+
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.country.code.upper() if self.country else "No Country")
 
@@ -202,22 +198,22 @@ class Site(models.Model):
     name = models.CharField(max_length=64, verbose_name='Site Name')
     address = models.TextField(blank=True, null=True)
     country = models.ForeignKey(Country, blank=True, null=True)
-    
+
     def as_dict(self, minimal=False):
         d = {
             'id': self.id,
             'name': self.name,
             'country': self.country.as_dict(),
-        }
+            }
         if not minimal:
             d['registrations'] = [r.as_dict(minimal=True) for r in Registration.objects.all()]
             d['procurements'] = [p.as_dict() for p in Procurement.objects.all()]
             d['manufacturer'] = self.manufacturer.as_dict(minimal=True)
         return d
-    
+
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.manufacturer.name)
-    
+
     class Meta:
         verbose_name = 'Manufacturing Site'
 
@@ -234,12 +230,12 @@ class Supplier(models.Model):
     email = models.EmailField(verbose_name='Email Address', blank=True, null=True)
     altemail = models.EmailField(verbose_name='Alternative Email Address', blank=True, null=True)
     authorized = models.BooleanField(verbose_name='Manufacturer Authorisation', default=False)
-    
+
     def is_manufacturer(self):
         if not self.manufacturer:
             return False
         return True
-    
+
     def as_dict(self, minimal=False, manufacturer=True):
         d = { 'id': self.id,
               'name': self.name,
@@ -250,7 +246,7 @@ class Supplier(models.Model):
               'email': self.email,
               'authorized': self.authorized }
         if self.country:
-              d['country'] = self.country.as_dict()
+            d['country'] = self.country.as_dict()
         if manufacturer:
             if self.manufacturer:
                 d['manufacturer'] = self.manufacturer.as_dict(minimal=True)
@@ -260,31 +256,31 @@ class Supplier(models.Model):
                        'altemail': self.altemail })
             d['registrations'] = [r.as_dict(minimal=True) for r in self.registration_set.all()]
             d['procurements'] = [{
-                    'id': p.id,
-                    'product': {
-                        'id': p.product.id,
-                        'name': p.product.name,
-                        'medicine': {
-                            'id': p.product.medicine.id,
-                            'dosageform': {
-                                'id': p.product.medicine.dosageform.id,
-                                'name': p.product.medicine.dosageform.name
-                                },
-                            'ingredients': [{
-                                    'inn': i.inn.name.title(),
-                                    'strength': i.strength
-                                    } for i in p.product.medicine.ingredient_set.all()]
-                            },
-                            'pack-size' : p.container.as_dict(minimal=True),
-                        },
-                    'country': {
-                        'id': p.country.id,
-                        'name': p.country.name
-                        }
-                    } for p in self.procurement_set.all()]
+                                     'id': p.id,
+                                     'product': {
+                                         'id': p.product.id,
+                                         'name': p.product.name,
+                                         'medicine': {
+                                             'id': p.product.medicine.id,
+                                             'dosageform': {
+                                                 'id': p.product.medicine.dosageform.id,
+                                                 'name': p.product.medicine.dosageform.name
+                                             },
+                                             'ingredients': [{
+                                                                 'inn': i.inn.name.title(),
+                                                                 'strength': i.strength
+                                                             } for i in p.product.medicine.ingredient_set.all()]
+                                         },
+                                         'pack-size' : p.container.as_dict(minimal=True),
+                                         },
+                                     'country': {
+                                         'id': p.country.id,
+                                         'name': p.country.name
+                                     }
+                                 } for p in self.procurement_set.all()]
             #d['procurements'] = [p.as_dict(minimal=True, supplier=False) for p in self.procurement_set.all()]
         return d
-    
+
     def __unicode__(self):
         return u'%s' % (self.name)
 
@@ -295,7 +291,7 @@ class Product(models.Model):
     site = models.ForeignKey(Site, verbose_name='Manufacturer Site', blank=True, null=True)
     generic = models.BooleanField(default=True)
     # TODO Need to add manufacturer to as_dict
-    
+
     def as_dict(self, medicine=True, minimal=False, registrations=True):
         d = { 'id': self.id,
               'name': self.name,
@@ -307,7 +303,7 @@ class Product(models.Model):
                 'name': self.manufacturer.name,
                 'country': self.manufacturer.country.name,
                 'site': self.site.name,
-            }
+                }
         if medicine:
             d['medicine'] = self.medicine.as_dict(minimal=True, products=False, procurements=False)
         if registrations:
@@ -315,7 +311,7 @@ class Product(models.Model):
         if not minimal:
             d['procurements'] = [r.as_dict() for r in self.procurement_set.all()]
         return d
-    
+
     def __unicode__(self):
         if self.name:
             return u'%s - %s (%s)' % (self.name, self.manufacturer, str(self.medicine))
@@ -327,16 +323,16 @@ class Container(models.Model):
     type = models.CharField(max_length=32, verbose_name='Container Type', help_text='This should be the actual type of containter that the medicine is distributed in eg. bottle, blister pack, tube etc.')
     unit = models.CharField(max_length=32, verbose_name='Basic Unit', help_text='This represents the basic unit of measure for this container eg. ml (for a bottled suspension), g (for a tube of ointment) or tablet/capsule (for a bottle of tablets/capsules).', blank=True)
     quantity = models.FloatField(verbose_name='Container size in basic units', help_text='The total container size eg. 100 (for a 100ml bottle), 50 (for a bottle of 50 tablets) or 3.5 (for a 3.5g tube of ointment).')
-    
+
     def as_dict(self, minimal=False):
         d = { 'type': self.type,
               'unit': self.unit,
               'quantity': self.quantity }
         return d
-    
+
     def __unicode__(self):
         return u'%.7g %s %s' % (self.quantity, self.unit, self.type)
-    
+
 
 class Registration(SourcedModel):
     country = models.ForeignKey(Country)
@@ -348,7 +344,7 @@ class Registration(SourcedModel):
     application = models.DateField(verbose_name='Application Date', blank=True, null=True)
     registered = models.DateField(verbose_name='Registration Date', blank=True, null=True)
     expiry = models.DateField(verbose_name='Registration Expiry Date', blank=True, null=True)
-    
+
     def as_dict(self, minimal=False, medicine=True, product=True):
         d = { 'id': self.id,
               'number': self.number,
@@ -368,7 +364,7 @@ class Registration(SourcedModel):
             if self.supplier:
                 d['supplier'] = self.supplier.as_dict(minimal=True)
         return d
-    
+
     def __unicode__(self):
         return u'%s - %s' % (self.number, self.product.name)
 
@@ -378,10 +374,10 @@ class Registration(SourcedModel):
 
 class Currency(models.Model):
     code = models.CharField(max_length=3, verbose_name='Currency Code', help_text='Enter the ISO 4217 currency code for the currency. This is a 3 letter code in all capitals eg. USD, ZAR etc.')
-    
+
     def __unicode__(self):
         return self.code
-    
+
     class Meta:
         verbose_name_plural = 'Currencies'
 
@@ -423,35 +419,41 @@ class Procurement(SourcedModel):
     method = models.CharField(max_length=100, verbose_name='Procurement Method', help_text='Open or restricted ICB, domestic tender, shopping, sole source.', blank=True, null=True)
     start_date = models.DateField(max_length=32, verbose_name='Period Start', help_text='This is the first day that the procurement price is valid for (may be left blank).', blank=True, null=True)
     end_date = models.DateField(max_length=32, verbose_name='Period End', help_text='This is the last day that the procurement price is valid for (may be left blank).', blank=True, null=True)
-    
+
     objects = ProcurementManager()
 
     def as_dict(self):
-        d = { 'id': self.id,
-              'incoterm': { 'id': self.incoterm.id,
-                            'name': self.incoterm.name,
-                            'description': self.incoterm.description},
-              'price': self.price,
-              'currency': { 'id': self.currency.id,
-                            'code': self.currency.code },
-              'price_usd': self.price_usd,
-              'volume': self.volume * (self.packsize or 1),
-              'method': self.method,
-              'country': { 'id': self.country.id,
-                           'code': self.country.code,
-                           'name': self.country.name },
-              'container': { 'id': self.container.id,
-                             'type': self.container.type,
-                             'unit': self.container.unit,
-                             'quantity': self.container.quantity },
-              'packsize': self.packsize }
-        #if self.pack:
-        #    d['pack'] = self.pack.as_dict()
+        d = {
+            'id': self.id,
+            'incoterm': { 'id': self.incoterm.id,
+                          'name': self.incoterm.name,
+                          'description': self.incoterm.description},
+            'price': self.price,
+            'currency': { 'id': self.currency.id,
+                          'code': self.currency.code },
+            'price_usd': self.price_usd,
+            'volume': self.volume * (self.packsize or 1),
+            'method': self.method,
+            'country': { 'id': self.country.id,
+                         'code': self.country.code,
+                         'name': self.country.name },
+            'container': { 'id': self.container.id,
+                           'type': self.container.type,
+                           'unit': self.container.unit,
+                           'quantity': self.container.quantity },
+            'packsize': self.packsize,
+            'source': {'name': self.source.name,
+                       'date': self.source.date.date().isoformat(),
+                       'url': self.source.url},
+            }
         if self.price_per_unit:
             d['price_per_unit'] = self.price_per_unit
         if self.product:
             d['product'] = { 'id': self.product.id,
-                             'name': self.product.name }
+                             'name': self.product.name,
+                             }
+            if self.product.medicine:
+                d['product']['medicine'] = self.product.medicine.as_dict(minimal=True, products=False, procurements=False)
         if self.start_date:
             d['start_date'] = self.start_date.isoformat()
         if self.end_date:
@@ -463,11 +465,11 @@ class Procurement(SourcedModel):
             d['manufacturer'] = {
                 'id': self.product.manufacturer.id,
                 'name': self.product.manufacturer.name,
-            }
+                }
             if self.product.manufacturer.country:
                 d["manufacturer"]["country"] = self.product.manufacturer.country.name,
         return d
-    
+
     @property
     def msh_ratio(self):
 
@@ -476,7 +478,7 @@ class Procurement(SourcedModel):
             return self.price_per_unit / msh_price
         else:
             return "N/A"
-        
+
     @property
     def price_usd(self):
         c = currency.models.Currency.objects.get(code=self.currency.code)
@@ -490,7 +492,7 @@ class Procurement(SourcedModel):
         if self.container.quantity:
             return self.price_usd / self.container.quantity
         return None
-    
+
     def __unicode__(self):
         if self.volume:
             return u'%d x %s' % (self.volume, self.product.__unicode__())
@@ -518,11 +520,11 @@ class Context(SourcedModel):
     local_preference = models.CharField(max_length=64, help_text='% figure and requirements applicable to local preference.', blank=True, null=True)
     import_duty = models.FloatField(max_length=64, verbose_name='Import duties and taxes', help_text='% figure as applicable.', blank=True, null=True)
     freight = models.FloatField(max_length=64, verbose_name='Average cost for freight & insurance', help_text='% figure.', blank=True, null=True)
-    
+
     def as_dict(self, minimal=False):
         d = {}
         return d
-    
+
     def __unicode__(self):
         return u'%d x %s' % (self.volume, self.product.name)
 
