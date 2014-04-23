1106be5457fa0a0670dd24c87252ebc68911768b
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-04-23 12:34:33 +0200

    Capture Medicine records from dump.

--- rebuild_db.py
+++ rebuild_db.py
@@ -1,16 +1,53 @@
 import backend.models as models
 from backend import db
+import json
 
 db.drop_all()
 db.create_all()
 
-tmp = models.Source()
-tmp.name = "Test Source"
-db.session.add(tmp)
+f = open("dump.json", "r")
+medicines = json.loads(f.read())
+f.close()
 
-tmp = models.Country()
-tmp.code = "RSA"
-tmp.name = "South Africa"
-db.session.add(tmp)
+print dir(medicines[0])
+
+for medicine in medicines:
+    
+    medicine_obj = models.Medicine()
+
+    # capture ingredients
+    for ingredient in medicine["ingredients"]:
+        component_obj = models.Component.query.filter(models.Component.name==ingredient["inn"]).first()
+        if component_obj is None:
+            component_obj = models.Component()
+            component_obj.name = ingredient["inn"]
+            db.session.add(component_obj)
+
+        ingredient_obj = models.Ingredient()
+        ingredient_obj.component = component_obj
+        ingredient_obj.strength = ingredient["strength"]
+        ingredient_obj.medicine = medicine_obj
+        db.session.add(ingredient_obj)
+    
+    db.session.add(medicine_obj)
+
+    # capture procurements
+    for procurement in medicine["procurements"]:
+        # capture country
+
+        # capture product
+
+        # capture manufacturer
+
+        # capture site
+
+        # capture supplier
+
+        pass
+
+    # capture MSH benchmark price
+
+    # capture dosage form
+
 
 db.session.commit()
