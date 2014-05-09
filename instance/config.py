0742cc263209e7840c21a24779cdc7652c55d539
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-05-09 14:11:07 +0200

    Revert "Run frontend and backend apps from runserver.py using WSGI dispatcher middleware."
    This reverts commit b71049667b7324f9aec5888f93143680ba541210.

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,7 @@
 import logging
+import json
 
-API_HOST = 'http://127.0.0.1:5000/api/'
+API_HOST = 'http://127.0.0.1:5000/'
 
 LOG_LEVEL = logging.DEBUG
 LOGGER_NAME = "med-db-logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
--- runserver.py
+++ runserver.py
@@ -1,16 +1,6 @@
-from werkzeug.wsgi import DispatcherMiddleware
-from werkzeug.serving import run_simple
+from backend import app
 
-from backend import app as backend_app
-from frontend import app as frontend_app
+if __name__ == "__main__":
 
-application = DispatcherMiddleware(
-    frontend_app,
-    {
-        '/api': backend_app,
-    }
-)
-
-if __name__ == '__main__':
-    run_simple('localhost', 5000, application,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+    # run Flask dev-server
+    app.run(port=5000)
