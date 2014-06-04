157d42e5cbed2cb99a293a4c1b2533ffb719bbbf
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-06-04 10:20:34 +0200

    Admin: Handle user registration, login & logout. Add custom index page.

--- instance/config.py
+++ instance/config.py
@@ -7,6 +7,6 @@ LOG_LEVEL = logging.DEBUG
 LOGGER_NAME = "med-db-logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/med-db.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/petrus/Desktop/code4sa/med-db/instance/med-db.db'
 
 RESULTS_PER_PAGE = 50
