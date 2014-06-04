46034658beb3043879a8f9f3c06854c80f96d17f
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-06-04 18:01:37 +0200

    Config: locat database with absolute path.

--- config/staging/config.py
+++ config/staging/config.py
@@ -6,7 +6,7 @@ LOG_LEVEL = logging.INFO
 LOGGER_NAME = "med-db-logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/med-db.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////var/www/med-db/instance/med-db.db'
 
 RESULTS_PER_PAGE = 50
 
