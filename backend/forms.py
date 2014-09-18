cb11412c55d683f0a75fc84a997ee60ab66c4754
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-09-18 16:04:31 +0200

    Procurement form: Add descriptions to fields.

--- backend/forms.py
+++ backend/forms.py
@@ -51,21 +51,18 @@ def get_supplier_choices():
 
 class ProcurementForm(Form):
 
-    def init(self):
-        country
-
-    country = fields.Select2Field('Country', [validators.InputRequired()], coerce=int, choices=country_choices)
-    product = fields.Select2Field('Product', [validators.InputRequired()], coerce=int)
-    supplier = fields.Select2Field('Supplier', [validators.InputRequired()], coerce=int)
-    container = SelectField('Container', choices=container_choices)
-    pack_size = IntegerField('Pack size', [validators.InputRequired()])
-    pack_price = FloatField('Pack price', [validators.InputRequired()])
-    currency = fields.Select2Field('Currency', [validators.InputRequired()], coerce=int, choices=currency_choices)
+    country = fields.Select2Field('Country', [validators.InputRequired()], coerce=int, choices=country_choices, description="The country that made the procurement.")
+    product = fields.Select2Field('Product', [validators.InputRequired()], coerce=int, description="Product = Medicine + Manufacturer + Site")
+    supplier = fields.Select2Field('Supplier', [validators.InputRequired()], coerce=int, description="The Supplier may be the same as the Manufacturer")
+    container = SelectField('Container', choices=container_choices, description="What is the smallest unit of packaging?")
+    pack_size = IntegerField('Pack size', [validators.InputRequired()], description="How many units are there in a pack?")
+    pack_price = FloatField('Pack price', [validators.InputRequired()], description="What is the price of on pack/bottle?")
+    currency = fields.Select2Field('Currency', [validators.InputRequired()], coerce=int, choices=currency_choices, description="Select the currency of the transaction.")
     pack_price_usd = FloatField('Pack price in USD', [validators.InputRequired()])
-    unit_price_usd = FloatField('Unit price in USD', [validators.InputRequired()])
-    quantity = IntegerField('Quantity', [validators.InputRequired()])
+    unit_price_usd = FloatField('Unit price in USD', [validators.InputRequired()], description="This is the price we use for making comparisons.")
+    quantity = IntegerField('Quantity', [validators.InputRequired()], description="How many packs were bought/contracted at this price?")
     method = SelectField('Method', choices=procurement_method_choices)
-    start_date = DateField('Start date', [validators.InputRequired()], format="%Y-%m-%d", widget=widgets.DatePickerWidget())
-    end_date = DateField('End date', [validators.InputRequired()], widget=widgets.DatePickerWidget())
+    start_date = DateField('Start date', [validators.InputRequired()], format="%Y-%m-%d", widget=widgets.DatePickerWidget(), description="From when was this price valid?")
+    end_date = DateField('End date', [validators.InputRequired()], widget=widgets.DatePickerWidget(), description="Until when does the price stay valid?")
     incoterm = SelectField('Incoterm', [validators.InputRequired()], coerce=int, choices=incoterm_choices)
 
